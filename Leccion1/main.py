38cf643c5d64aa79682e94709e46907bc9889b2a
UrojasG <urielelihurojasgarfias@gmail.com>
2023-02-15 14:29:02 -0600

    Leccion 1

---
+++ Leccion1/main.py
@@ -0,0 +1,11 @@
+##First hello world
+print("Hello world from python")
+
+
+# Excercise #1
+print("This is my greeting from python")
+
+#declare a variable
+MyVar = "Hello from python"
+MyVar = 32
+print(MyVar)
