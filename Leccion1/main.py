fdc7368f6b853fe44974481a71927b0e3e06dae7
UrojasG <urielelihurojasgarfias@gmail.com>
2023-02-15 15:04:46 -0600

    Leccion2

--- Leccion1/main.py
+++ Leccion1/main.py
@@ -1,11 +1,6 @@
 ##First hello world
 print("Hello world from python")
 
-
 # Excercise #1
 print("This is my greeting from python")
 
-#declare a variable
-MyVar = "Hello from python"
-MyVar = 32
-print(MyVar)
---
+++ Leccion2/__init__.py
@@ -0,0 +1,24 @@
+# declare a variable
+MyVar = "Hello from python"
+MyVar = 32
+print(MyVar)
+
+# Example for var declaration with sum
+x = 10
+y = 2
+z = x + y
+print(x, '\n', y, '\n', z)
+
+#Visualize memory position of a var
+print(id(x))
+
+
+
+#Exercise: declare 3 and asing a values of name, tel number an email
+Name = "Uriel ELihu"
+TelNumber = '5530737755'
+email = 'urielcahua@gmail.com'
+
+print (Name, TelNumber, email)
+
+
